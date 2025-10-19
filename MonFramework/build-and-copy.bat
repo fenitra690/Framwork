5f44827a7a64c3e6f7a4b3a44dd71355807502a7
fenitra690 <andriamanambinafenitra@gmail.com>
2025-10-19 17:03:24 +0200

    sprinte1bis
    sprinte1bis

--- MonFramework/build-and-copy.bat
+++
@@ -1,6 +0,0 @@
-REM Compile le projet et génère le jar
-call mvn clean package
-if errorlevel 1 exit /b 1
-
-REM Copier le jar généré vers ../framework-test/lib/
-copy target\framework-sprint-1.jar ..\MonProjetTest\WEB-INF\lib\
